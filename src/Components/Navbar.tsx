b623a3872dfe939f5609530c8b2065e1582de0bd
Christian <kakcsi02@gmail.com>
2025-12-15 20:49:57 +0100

    Commit
    Commit

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -31,7 +31,7 @@ export default function Navbar() {
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex shrink-0 items-center">
                   <img
-                    src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+                    src="../logo.png"
                     alt="Logo"
                     className="h-8 w-auto"
                   />
